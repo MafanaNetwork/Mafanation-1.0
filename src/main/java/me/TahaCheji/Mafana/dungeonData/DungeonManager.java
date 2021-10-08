08dd434efceecea744f346adb039464b9309a470
MaskedWasTaken <cctaha76@gmail.com>
2021-10-07 22:36:21 -0400

    e

---
+++ src/main/java/me/TahaCheji/Mafana/dungeonData/DungeonManager.java
@@ -0,0 +1,12 @@
+package me.TahaCheji.Mafana.dungeonData;
+
+public interface DungeonManager {
+
+    //start dungeon
+    //spawn mobs
+    //spawn boss
+    //end dungeon
+    //about of kills to get to get to boss
+    //
+
+}
